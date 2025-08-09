1b1f98fcb90b62cfd8663283ff77968aacd568f7
surafans <xjupiter09@gmail.com>
2025-08-09 21:06:12 +0530

    Update firebase.js

--- sura-login/firebase.js
+++ sura-login/firebase.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +12,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
