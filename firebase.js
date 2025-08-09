01f41eac81c9e999e575b6d2cd0e193675426626
surafans <xjupiter09@gmail.com>
2025-08-09 22:13:30 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Is9ekckvL3otvCDbz90plvEvlPBRDT4",
+  authDomain: "sura-fans-auth.firebaseapp.com",
+  projectId: "sura-fans-auth",
+  storageBucket: "sura-fans-auth.firebasestorage.app",
+  messagingSenderId: "399675179284",
+  appId: "1:399675179284:web:b4e7b2d0a409c2eab34201"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
